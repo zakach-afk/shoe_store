00745f985c5da4dd2e947fb0e8133b16952ae54b
zakach-afk <zakach6867@gmail.com>
2026-08-20 23:03:51 -1200

    Fix media serving in production when DEBUG is False

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),  # Routing homepage requests to products app
-]
-
-# Serves both static files (CSS/JS/images) and media uploads during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('products.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
