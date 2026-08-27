702a5ae6ea9c92b19ff280587bbc4acc4d77ee23
zakach-afk <zakach6867@gmail.com>
2026-08-26 19:29:39 -1200

    Configure EMAIL_* settings in config/settings.py for order email alerts

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email Configuration
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'warisali942015@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # Unfold Modern Admin Settings
 UNFOLD = {
     "SITE_TITLE": "SOLO FOOTWEAR Control Center",
